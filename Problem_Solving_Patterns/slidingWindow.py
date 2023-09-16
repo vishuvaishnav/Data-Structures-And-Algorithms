b645eebc85b76b560d414dcc88526f7b891cace3
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-09-16 18:00:20 +0530

    Create slidingWindow.py

---
+++ Problem_Solving_Patterns/slidingWindow.py
@@ -0,0 +1,16 @@
+def maxSubarraySum(arr,num):
+    maxsum = 0
+    tempsum = 0
+    if len(arr)<num:
+        return False
+    for i in range(0,num):
+        maxsum+=arr[i]
+    tempsum=maxsum
+    for i in range(num,len(arr)):
+        tempsum = tempsum - arr[i-num] + arr[i]
+        maxsum=max(maxsum,tempsum)
+    
+    return maxsum  
+
+#printLine
+print(maxSubarraySum([2,1,2,5,8,6],3))
