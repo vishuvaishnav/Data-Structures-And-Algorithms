a48c3e2dce377f681d5b4d79cc9803fbbe443af5
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-09-16 17:56:50 +0530

    Delete LinearSearch.py

--- LinearSearch.py
+++
@@ -1,9 +0,0 @@
-arr = list(map(int,input("Enter the list").split()))
-num = int(input("Enter the number to search "))
-
-for i in arr:
-  if i==num:
-    print("Found at index",arr.index(num))
-    break
-else:
-  print("Not Found")
