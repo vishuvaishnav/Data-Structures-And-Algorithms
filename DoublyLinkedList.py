ccbe4b73dc20af25fca92ad88cb47516e8c78360
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-09-23 14:55:05 +0530

    Create DoublyLinkedList.py

---
+++ DoublyLinkedList.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.next = None
+        self.prev = None
+        
+class doublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def push(self,item):
+        newNode = Node(item)
+        if(self.length == 0):
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        self.length += 1
+        return self.length
+    def pop(self):
+        if self.length == 0:
+            return "Empty linked list"
+        poppedNode = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = poppedNode.prev
+            self.tail.next = None
+            poppedNode.prev = None
+        self.length -= 1
+        return poppedNode
+    def display(self):
+        if self.head is None:
+            print("The list is empty")
+            return 0
+        else:
+            new = self.head
+            while new is not None:
+                print("Element is: ", new.item)
+                new = new.next
+        print("\n")
+        return 0
+    
+ll = doublyLinkedList()
+ll.push(10)
+ll.push(20)
+ll.push(30)
+ll.push(40)
+ll.push(50)
+ll.display()
+ll.pop()
+ll.pop()
+ll.display()
+    
