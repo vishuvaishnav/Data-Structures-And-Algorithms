1bb5c5914282f930e47329472afc9f4b9fd92c22
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-09-23 17:22:37 +0530

    Update DoublyLinkedList.py

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -9,6 +9,8 @@ class doublyLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+        
+    #push iteams at the back just like in stack
     def push(self,item):
         newNode = Node(item)
         if(self.length == 0):
@@ -20,6 +22,8 @@ class doublyLinkedList:
             self.tail = newNode
         self.length += 1
         return self.length
+    
+    #pop item from the back
     def pop(self):
         if self.length == 0:
             return "Empty linked list"
@@ -33,6 +37,36 @@ class doublyLinkedList:
             poppedNode.prev = None
         self.length -= 1
         return poppedNode
+        
+    # Remove the head of linked list 
+    def shift(self):
+        if self.length == 0:
+            return "Empty List"
+        oldHead = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:    
+            self.head = oldHead.next
+            self.head.prev = None
+            oldHead.next = None
+        self.length -= 1
+        return oldHead
+        
+    #adding the element to the head of the linked list
+    def unshift(self,value):
+        newNode = Node(value)
+        if(self.length == 0):
+            self.head = newNode
+            self.tail = newNode
+        else :
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+        self.length += 1
+        return self.length
+        
+    #display out doubly linked list
     def display(self):
         if self.head is None:
             print("The list is empty")
@@ -53,6 +87,9 @@ ll.push(40)
 ll.push(50)
 ll.display()
 ll.pop()
-ll.pop()
 ll.display()
-    
+ll.shift()
+ll.display()
+ll.unshift(100)
+ll.display()
+
