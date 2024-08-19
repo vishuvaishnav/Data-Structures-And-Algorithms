c759d72152bc13045f5df31ac4beec6eec1ea61f
Vishu Vaishnav <76559999+vishuvaishnav@users.noreply.github.com>
2024-08-19 12:51:56 +0530

    Update nextPermutation.py

--- Problem_Solving_Patterns/nextPermutation.py
+++ Problem_Solving_Patterns/nextPermutation.py
@@ -5,24 +5,22 @@ def next_permutation(arr,n):
             k = i-1
             break
     if k == -1:
-        arr.sort()
+        arr.reverse()
         return(arr)
-    else:
-        for i in range(n-1,k-1,-1):
-            if arr[i]>arr[k] :
-                arr[i],arr[k] = arr[k],arr[i]
-                break
-        #print(arr)
-        left = k+1 
-        right = n-1
-        while (left < right):
-            temp = arr[left]
-            arr[left] = arr[right]
-            arr[right] = temp
-            left += 1
-            right -= 1
-        
-        return(arr)
+    for i in range(n-1,k,-1):
+        if arr[i]>arr[k] :
+            arr[i],arr[k] = arr[k],arr[i]
+            break
+    left = k+1 
+    right = n-1
+    while (left < right):
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+        left += 1
+        right -= 1
+    
+    return(arr)
 
 arr = [2,1,5,4,3,0,0]
 n = len(arr)
